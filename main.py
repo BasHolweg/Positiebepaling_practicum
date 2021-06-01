794fb2deb2c72fcb2080cceff1c6ac6b9e52c4af
BasHolweg <b.holweg5@gmail.com>
2021-06-01 16:48:21 +0200

    added geometry and plotter

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import plotter as p
+import geometry as g
 
 
-# p1 = [x,y]
-
-
-def create_grid(x_size, y_size, z_size=None):
+def create_grid(x_size=10, y_size=10, z_size=None):
     c = []
-    for x in range(x_size):
-        for y in range(y_size):
+    for x in range(x_size + 1):
+        for y in range(y_size + 1):
             if z_size:
-                for z in range(z_size):
+                for z in range(z_size + 1):
                     c.append([x, y, z])
             else:
                 c.append([x, y])
     return np.array(c)
 
 
-coordinates = create_grid(60, 40)
-print(coordinates)
-print(coordinates[2399])
+def array_to_point_dist(array, point):
+    dist_array = []
+    for i in range(len(array)):
+        dist_array.append(g.distance(array[i], point))
+    return dist_array
+
+
+coordinates = create_grid(60,40)
+b1 = np.array([10,16])
+dist_array = array_to_point_dist(coordinates, b1)
+print(len(dist_array))
 print(len(coordinates))
+p.plot_diff(coordinates, dist_array)
---
+++ plotter.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_points(points, color='red', size=5):
+    fig = plt.figure()
+    if np.ndim(points) > 1:
+        if len(points[0]) == 3:
+            ax = plt.axes(projection='3d')
+            for i in range(len(points)):
+                ax.scatter(points[i][0], points[i][1], points[i][2], c=color, s=size)
+        else:
+            for i in range(len(points)):
+                plt.scatter(points[i][0], points[i][1], c=color, s=size)
+    else:
+        if len(points) == 3:
+            ax = plt.axes(projection='3d')
+            ax.scatter(points[0], points[1], points[2], c=color, s=size)
+        else:
+            plt.scatter(points[0], points[1], c=color, s=size)
+    plt.show()
+
+
+def plot_diff(points, distarray):
+    x, y, z = [], [], []
+    for i in range(len(points)):
+        x.append(points[i][0])
+        y.append(points[i][1])
+        z.append(distarray[i])
+    x, y, z = np.array(x), np.array(y), np.array(z)
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    surf = ax.plot_trisurf(x, y, z, cmap='inferno', linewidth=0.1)
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.show()
